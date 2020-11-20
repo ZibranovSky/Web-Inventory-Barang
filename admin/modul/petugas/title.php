1b0222d7823b91711213a608f71bb25dc00ab382
Muhamad Zibran Fitadiyatama <katyushachan191202@gmail.com>
2020-11-20 17:48:44 +0700

    Update title.php

--- admin/modul/petugas/title.php
+++ admin/modul/petugas/title.php
@@ -110,7 +110,7 @@
       <div id="page-wrapper">
         <div class="row">
           <div class="col-lg-12">
-            <h1 class="page-header">Data Rak</h1>
+            <h1 class="page-header">Data Petugas</h1>
           </div>
         </div>
 
