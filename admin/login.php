98d37763631a66f80d30b3455fb1213e43e53e0e
ALFIANBANTENG <aferdiansyah800@gmail.com>
2024-07-10 11:43:34 +0700

    Tampilan beranda admin

--- admin/login.php
+++ admin/login.php
@@ -26,7 +26,7 @@
     <nav class="navbar navbar-default navbar-fixed-top navbar-custom">
       <div class="container">
         <div class="navbar-header page-scroll">
-          <a class="navbar-brand">Login Adm Inventory</a>
+          <a class="navbar-brand">Login Admin Inventory</a>
         </div>
       </div>
     </nav>
