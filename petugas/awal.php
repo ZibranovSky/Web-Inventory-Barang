f83a43aeca1564782247b4e49adf08dcd61f54c5
umam <Umamselaluhappy@gmail.com>
2024-07-10 21:51:46 +0700

    update sidebar to horizontal

--- petugas/awal.php
+++ petugas/awal.php
@@ -64,35 +64,32 @@ if ( !isset($_SESSION["idinv2"])) {
           </ul>
 
         <!-- menu samping -->
-        <div class="navbar-default sidebar" role="navigation">
-          <div class="sidebar-nav navbar-collapse">
-            <ul class="nav" id="side-menu">
-              <li>
-                <a href="?m=awal.php">
-                  <i class="fa fa-dashboard"></i> Beranda
-                </a>
-              </li>
-              <li>
-                <a href="?m=barangMasuk&s=awal">
-                  <i class="fa fa-cart-arrow-down"></i> Data Barang Masuk
-                </a>
-              </li>
-                            
-                            <li>
-                <a href="?m=ajuan&s=awal">
-                  <i class="fa fa-gift"></i> Data Ajuan
-                </a>
-              </li>
-              <li>
-                <a href="logout.php" onclick="return confirm('yakin ingin logout?');">
-                  <i class="fa fa-warning"></i> Logout
-                </a>
-              </li>
-              
-            </ul>
-          </div>
-        </div>
-
+        <div class="navbar-default sidebar-horizontal" role="navigation">
+                <div class="navbar-collapse">
+                    <ul class="nav">
+                        <li>
+                            <a href="?m=awal.php">
+                                <i class="fa fa-dashboard"></i> Beranda
+                            </a>
+                        </li>
+                        <li>
+                            <a href="?m=barangMasuk&s=awal">
+                                <i class="fa fa-cart-arrow-down"></i> Data Barang Masuk
+                            </a>
+                        </li>
+                        <li>
+                            <a href="?m=ajuan&s=awal">
+                                <i class="fa fa-gift"></i> Data Ajuan
+                            </a>
+                        </li>
+                        <li>
+                            <a href="logout.php" onclick="return confirm('yakin ingin logout?');">
+                                <i class="fa fa-warning"></i> Logout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
       </nav>
 
       <div id="page-wrapper">
