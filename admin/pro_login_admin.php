616a65c4ab4d85322e84b7a9b601400496d2b6f5
ShaflyKP <146298229+Xaxisss@users.noreply.github.com>
2024-07-17 15:31:02 +0700

    mengamankan login admin dari SQL injection

--- admin/pro_login_admin.php
+++ admin/pro_login_admin.php
@@ -1,28 +1,37 @@
 <?php 
 include '../koneksi.php';
+
+// Ambil data dari form login
 $user = $_POST['user'];
 $pass = md5($_POST['pass']);
-$sql = "SELECT * FROM tb_admin WHERE username = '$user' AND password =  '$pass'";
-$login = mysqli_query($koneksi, $sql);
-$ketemu = mysqli_num_rows($login);
-$b = mysqli_fetch_array($login);
 
-if ($ketemu>0) {
-	session_start();
-	$_SESSION['idinv'] = $b['id_admin'];
-	$_SESSION['userinv'] = $b['username'];
-	$_SESSION['passinv'] = $b['password'];
-	$_SESSION['namainv'] = $b['nama'];
-	$_SESSION['teleponinv'] = $b['telepon'];
-	$_SESSION['fotoinv'] = $b['foto'];
-	header("location: index.php?m=awal");
-}else{
-	 echo '<script language="javascript">';
-        echo 'alert ("Username/Password ada yang salah, atau akun anda belum Aktif")';
-    echo '</script>';
-    header("location: login.php");
-   ;
+// Buat prepared statement
+$stmt = $koneksi->prepare("SELECT * FROM tb_admin WHERE username = ? AND password = ?");
+$stmt->bind_param("ss", $user, $pass);
+
+// Jalankan statement
+$stmt->execute();
+
+// Ambil hasil query
+$result = $stmt->get_result();
+$ketemu = $result->num_rows;
+$b = $result->fetch_array(MYSQLI_ASSOC);
+
+if ($ketemu > 0) {
+    session_start();
+    $_SESSION['idinv'] = $b['id_admin'];
+    $_SESSION['userinv'] = $b['username'];
+    $_SESSION['passinv'] = $b['password'];
+    $_SESSION['namainv'] = $b['nama'];
+    $_SESSION['teleponinv'] = $b['telepon'];
+    $_SESSION['fotoinv'] = $b['foto'];
+    header("Location: index.php?m=awal");
+    exit();
+} else {
+	echo "<script>
+	alert('Username atau Password salah');
+	window.location.href='login.php';
+  </script>";
+    exit();
 }
- 
-
- ?>
+?>
