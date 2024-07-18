d7da06b99b81b6c3a553218130be67becf3f3aaa
ShaflyKP <146298229+Xaxisss@users.noreply.github.com>
2024-07-18 16:30:02 +0700

    Revert "Update keamanan login admin"
    This reverts commit a71ea8f447da94e8965ff345933cf19c0772b485.

--- admin/pro_login_admin.php
+++ admin/pro_login_admin.php
@@ -1,39 +1,37 @@
-<?php
-session_start();
-include 'config.php'; // Pastikan Anda telah mengatur koneksi database di file config.php
+<?php 
+include '../koneksi.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['user'];
-    $password = $_POST['pass'];
+// Ambil data dari form login
+$user = $_POST['user'];
+$pass = md5($_POST['pass']);
 
-    try {
-        // Buat koneksi menggunakan PDO
-        $pdo = new PDO($dsn, $db_username, $db_password, $options);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Buat prepared statement
+$stmt = $koneksi->prepare("SELECT * FROM tb_admin WHERE username = ? AND password = ?");
+$stmt->bind_param("ss", $user, $pass);
 
-        // Query SQL menggunakan prepared statements
-        $stmt = $pdo->prepare("SELECT * FROM admins WHERE username = :username AND password = :password");
-        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
-        $stmt->execute();
+// Jalankan statement
+$stmt->execute();
 
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+// Ambil hasil query
+$result = $stmt->get_result();
+$ketemu = $result->num_rows;
+$b = $result->fetch_array(MYSQLI_ASSOC);
 
-        if ($user) {
-            // Login sukses, simpan data user di sesi
-            $_SESSION['user'] = $user;
-            header("Location: admin_dashboard.php"); // Ganti dengan halaman dashboard admin Anda
-            exit();
-        } else {
-            // Login gagal, kembali ke halaman login
-            header("Location: login_admin.php?error=1");
-            exit();
-        }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
+if ($ketemu > 0) {
+    session_start();
+    $_SESSION['idinv'] = $b['id_admin'];
+    $_SESSION['userinv'] = $b['username'];
+    $_SESSION['passinv'] = $b['password'];
+    $_SESSION['namainv'] = $b['nama'];
+    $_SESSION['teleponinv'] = $b['telepon'];
+    $_SESSION['fotoinv'] = $b['foto'];
+    header("Location: index.php?m=awal");
+    exit();
 } else {
-    header("Location: login_admin.php");
+	echo "<script>
+	alert('Username atau Password salah');
+	window.location.href='login.php';
+  </script>";
     exit();
 }
 ?>
